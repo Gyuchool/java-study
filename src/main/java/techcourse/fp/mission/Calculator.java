4844414398967758df888bf69ce7aa016225ee1e
giron <ggyucheol0@gmail.com>
2022-03-25 11:56:37 +0900

    refactor: lambda practice apply

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -5,28 +5,24 @@ import java.util.List;
 public class Calculator {
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream()
+                .mapToInt(number -> number)
+                .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .mapToInt(number -> number)
+                .sum();
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-
+        return numbers.stream()
+                .filter(number -> number >3)
+                .mapToInt(number -> number)
+                .sum();
         //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
 
-        return total;
     }
 }
